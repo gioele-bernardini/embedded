942a5c04346ab24139fb28ce7f623c9ac79ffed6
gioele-bernardini <gioele.g@outlook.com>
2024-07-13 12:32:56 +0200

    button up

--- examples/button_debounce/plot.c
+++ examples/button_debounce/plot.c
@@ -1,11 +1,10 @@
-// Code for priting a plot on the cli
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 
 #define N_COLUMNS 130
+#define N_ROWS 5
 
 void set_default(int* array) {
   for (int i = 0; i < N_COLUMNS; i++)
@@ -18,17 +17,29 @@ void set_custom(int* array) {
   }
 }
 
-void print_array(int* array) {
-  for (int i = 0; i < N_COLUMNS; i++)
-    printf("%d", array[i]);
+void print_column(int* array, int index) {
+  for (int i = 0; i < N_ROWS; i++) {
+    if (array[index] == 1) {
+      printf("*");
+    } else {
+      printf(" ");
+    }
+    printf("\n");
+  }
+}
 
+void print_matrix(int* array) {
+  for (int i = 0; i < N_COLUMNS; i++) {
+    print_column(array, i);
+  }
   printf("\n");
 }
 
 void update_forward(int* array) {
-  for (int i = 0; i < N_COLUMNS -1; i++) {
-    array[i] = array[i +1];
+  for (int i = 0; i < N_COLUMNS - 1; i++) {
+    array[i] = array[i + 1];
   }
+  array[N_COLUMNS - 1] = rand() % 2; // Randomize the last element to create a moving effect
 }
 
 int main() {
@@ -37,14 +48,12 @@ int main() {
 
   set_custom(array);
 
-  while(1) {
-    print_array(array);
-
+  while (1) {
+    print_matrix(array);
+    usleep(200000); // Sleep for 200 milliseconds
     system("clear");
-    sleep(0.2);
     update_forward(array);
   }
 
   return 0; // should NOT happen
 }
-
