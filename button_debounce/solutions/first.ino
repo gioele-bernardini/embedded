2a6f932d44c748b42a3bdff7cd4a3560a009ee0d
gioele-bernardini <gioele.g@outlook.com>
2024-07-14 15:37:26 +0200

    first solution completed.

--- button_debounce/solutions/first.ino
+++ button_debounce/solutions/first.ino
@@ -3,41 +3,42 @@ const int ledPin = 13;
 // The debounce time; increase if the output flickers
 const unsigned long debounceDelay = 50;
 
+int buttonPressCount = 0;
 int buttonState = 0;
-int lastButtonState = 0;
+int lastReading = 0;  // This was lastButtonState
 // The last time the output pin was toggled
 unsigned long lastDebounceTime = 0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
-  // initialize serial communication:
+  // Initialize serial communication:
   Serial.begin(9600);
 }
 
 void loop() {
   int reading = digitalRead(buttonPin);
 
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
+  if (reading != lastReading) {
+    lastDebounceTime = millis();  // Reset debounce timer on state change
   }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
     if (reading != buttonState) {
-      buttonState = reading;
+      buttonState = reading;  // Update button state
 
-      // toggle the LED based on the button state
+      // Toggle the LED based on the button state
       if (buttonState == HIGH) {
         digitalWrite(ledPin, HIGH);
-        Serial.println("1");
+        buttonPressCount++;
+        Serial.println(buttonPressCount);
       } else {
         digitalWrite(ledPin, LOW);
       }
     }
   }
 
-  // Save the reading. Next time through the loop, 
-  // it'll be the lastButtonState:
-  lastButtonState = reading;
+  // save reading for next interation
+  lastReading = reading;
 }
 
